536e0ab4773f3c3d9456d2b2aaf809d1e2589538
ajea <smithajea@rocketmail.com>
2023-07-06 16:13:59 -0400

    testing branch

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,60 +4,61 @@ import CardList from "../components/CardList";
 import SearchBar from "../components/SearchBar";
 import Scroll from "../components/Scroll";
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchTerm: "",
-		};
-	}
+// class App extends Component {
+// 	constructor() {
+// 		super();
+// 		this.state = {
+// 			robots: [],
+// 			searchTerm: "",
+// 		};
+// 	}
 
-	componentDidMount() {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then((resp) => {
-				return resp.json();
-			})
-			.then((users) => {
-				this.setState({ robots: users });
-			})
-			.catch((err) => {
-				console.log("Error with API", err.message);
-			});
-	}
-	onSearchChange = (event) => {
-		this.setState({ searchTerm: event.target.value });
-	};
+// 	componentDidMount() {
+// 		fetch("https://jsonplaceholder.typicode.com/users")
+// 			.then((resp) => {
+// 				return resp.json();
+// 			})
+// 			.then((users) => {
+// 				this.setState({ robots: users });
+// 			})
+// 			.catch((err) => {
+// 				console.log("Error with API", err.message);
+// 			});
+// 	}
 
-	render() {
-		const filteredRobots = this.state.robots.filter((robot) => {
-			return robot.name
-				.toLowerCase()
-				.includes(this.state.searchTerm.toLowerCase());
-		});
-		if (this.state.robots.length === 0) {
-			return (
-				<h1 className="f1 tc" style={{ color: "white" }}>
-					Loading....
-				</h1>
-			);
-		} else {
-			return (
-				<div className="tc">
-					<h1 className="f1" style={{ color: "white" }}>
-						RoboFriends
-					</h1>
-					<SearchBar
-						searchTerm={this.state.searchTerm}
-						onSearchChange={this.onSearchChange}
-					/>
-					<Scroll>
-						<CardList robots={filteredRobots} />
-					</Scroll>
-				</div>
-			);
-		}
-	}
-}
+// 	onSearchChange = (event) => {
+// 		this.setState({ searchTerm: event.target.value });
+// 	};
 
-export default App;
+// 	render() {
+// 		const filteredRobots = this.state.robots.filter((robot) => {
+// 			return robot.name
+// 				.toLowerCase()
+// 				.includes(this.state.searchTerm.toLowerCase());
+// 		});
+// 		if (this.state.robots.length === 0) {
+// 			return (
+// 				<h1 className="f1 tc" style={{ color: "white" }}>
+// 					Loading....
+// 				</h1>
+// 			);
+// 		} else {
+// 			return (
+// 				<div className="tc">
+// 					<h1 className="f1" style={{ color: "white" }}>
+// 						RoboFriends
+// 					</h1>
+// 					<SearchBar
+// 						searchTerm={this.state.searchTerm}
+// 						onSearchChange={this.onSearchChange}
+// 					/>
+// 					<Scroll>
+// 						<CardList robots={filteredRobots} />
+// 					</Scroll>
+// 				</div>
+// 			);
+// 		}
+// 	}
+// }
+
+// export default App;
