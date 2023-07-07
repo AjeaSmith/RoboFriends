b4f6a2e23bea46b12452f92f5d4263f6d600ab0b
ajea <smithajea@rocketmail.com>
2023-07-07 08:57:49 -0400

    converted App.js to hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,59 @@
-import React, { Component } from "react";
-import CardList from "../components/CardList";
+import React, { useState, useEffect } from "react";
 
+import CardList from "../components/CardList";
 import SearchBar from "../components/SearchBar";
 import Scroll from "../components/Scroll";
+
+const App = () => {
+	const [robots, setRobots] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const onSearchChange = (event) => {
+		setSearchTerm(event.target.value);
+	};
+	const filteredRobots = robots.filter((robot) => {
+		return robot.name.toLowerCase().includes(searchTerm.toLowerCase());
+	});
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((resp) => {
+				return resp.json();
+			})
+			.then((users) => {
+				setRobots(users);
+			})
+			.catch((err) => {
+				console.log("Error with API", err.message);
+			});
+	}, []);
+
+	return (
+		<div>
+			{/* Error boundary goes here... */}
+			{robots.length === 0 ? (
+				<h1 className="f1 tc" style={{ color: "white" }}>
+					Loading....
+				</h1>
+			) : (
+				<div className="tc">
+					<h1 className="f1" style={{ color: "white" }}>
+						RoboFriends
+					</h1>
+					<SearchBar
+						searchTerm={searchTerm}
+						onSearchChange={onSearchChange}
+					/>
+					<Scroll>
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				</div>
+			)}
+		</div>
+	);
+};
+
+// export default App;
 
 // class App extends Component {
 // 	constructor() {
@@ -61,4 +112,4 @@ import Scroll from "../components/Scroll";
 // 	}
 // }
 
-// export default App;
+export default App;
